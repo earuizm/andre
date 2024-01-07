0fb893e0bdfc0f0648905694bcfbf50860286628
andre <121254662+earuizm@users.noreply.github.com>
2024-01-06 22:10:08 -0800

    feat: final push

--- andre-webapp/src/App.js
+++ andre-webapp/src/App.js
@@ -11,9 +11,11 @@ import Pet from './Pet.jsx';
 const App = () => {
   const deskPosition = [-10, -31, 2];
   const [textSide, setTextSide] = useState("hi, welcome to my website my name is eberto andre ruiz");
+  const [selectedImages, setSelectedImages] = useState([]);
 
-  const changeText = (text) => {
+  const changeText = (text, images) => {
     setTextSide(text)
+    setSelectedImages(images);
   }
 
   return (
@@ -23,9 +25,27 @@ const App = () => {
         <ambientLight intensity={Math.PI / 2} />
         <group scale={20} position={[0, -200, -500]}>
           {/* <Desk position={deskPosition} /> */}
-          <Computer onClick={() => changeText("hello from computer")} position={[-13, 0, 4]} />
-          <Headphones onClick={() => changeText("hello from headphones")} position={[-3, 0, -13]} scale={2} />
-          <Pet onClick={() => changeText("hello from pet")}  position={[8, 0, 8]} scale={2} />
+          <Computer onClick={() => changeText(
+              <pre>
+              {`i graduated from Pomona College with a computer science degree\n\ni interned at Visa in the San Francisco Bay Are in \nmy senior year of college\n\ni then converted to a full time and worked there for 2 years building\nautomation tools for the Test Engineering team, while leading the effort of creating\nVisa's first datalake\n\ni now work at Intuit in the San Francisco Bay Area on the Data team building tools\nthat have revolutionized engineers' day to day\nthough automation and Intuit's own genai\n\ni am proficient in react, java, python, sql\n\ni have extensive experience with databases, rest apis/graphql and full stack applications\nand i am very familiar with cloud platform such as aws and gcp`}
+            </pre>
+            ,[]
+          )} position={[-13, 0, 4]} />
+          <Headphones onClick={() => changeText(
+              <pre>
+              {`i love listening to music\ni listened to 47,105 minutes of music last year on spotify\n\nmy top artists where\nfeid\npinkpantheress\nclairo\nrauw alejandro\nkali uchis\n\ncurrently my favorite song is space cadet by beabadoobee   
+              `}
+            </pre>
+            ,[]
+            )} position={[-3, 0, -13]} scale={2} />
+          <Pet onClick={() => changeText(
+            <pre>
+              {`in twenty twenty three i got a cat named ash\n\nshe lived in a farm in florida and was at risk of being killed\n\ni flew to florida and was able to bring her to california\n\nshe is the most loving and silly little girl
+              `}
+            </pre>,
+            ['/images/ash1.jpg','/images/ash2.jpg']
+          )} position={[8, 0, 8]} scale={2} />
+
           <Text
             color="black"
             fontSize={2}
@@ -46,9 +66,22 @@ const App = () => {
         <Environment preset="city" background blur={1} />
         <PerspectiveCamera makeDefault position={[0, 0, 18.5]} />
       </Canvas>
-      <div style={{ flex: 1 }}>
-        <p>{textSide}</p>
-      </div>
+      <div style={{ height: '100vh', overflowY: 'scroll' }}>
+  <div style={{ overflowY: 'hidden', marginBottom: '10px' }}>
+    <p>{textSide}</p>
+  </div>
+  <div style={{ display: 'flex', flexDirection: 'column' }}>
+    {selectedImages.map((image, index) => (
+      <img
+        key={index}
+        src={image}
+        alt={`Pet ${index + 1}`}
+        style={{ width: '300px', height: '350px', marginBottom: '10px' }}
+      />
+    ))}
+  </div>
+</div>
+
     </div>
   );
 };
